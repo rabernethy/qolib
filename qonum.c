807e4230f7280629484935eb612762c5bff17d57
Russell Abernethy <55411746+rabernethy@users.noreply.github.com>
2020-08-31 15:16:40 -0400

    changed prime number calculation
    When searching for if a number is prime or not, you only need to check up to the square root of the possible number. Look at The Sieve of Eratosthenes for more on explanation.

--- qonum.c
+++ qonum.c
@@ -1,13 +1,13 @@
 #include "qonum.h"
 #include "constants.h"
 #include <stdlib.h>
-
+#include <math.h>
 int isPrime(unsigned long long possiblePrime) {
 	if(possiblePrime % 2 == 0) {
 		return False;
 	}
 
-	for(unsigned long x = 3; x < possiblePrime/2; x+=2) {
+	for(unsigned long x = 3; x < sqrt(possiblePrime); x+=2) {
 		if(possiblePrime % x == 0) {
 			return False;
 		}
